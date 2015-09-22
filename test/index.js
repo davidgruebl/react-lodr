0187bfe749ff68eabdfd89e58d6cc5f35ee92520
David Thomas Grübl <me@davidgruebl.com>
2015-09-21 23:57:52 -0700

    init

---
+++ test/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var should = require('should');
+var react-lodr = require('../');
+
+describe('FUNCTIONNAME()', function() {
+  it('should TASKNAME', function(done) {
+    should.exist(react-lodr);
+    done();
+  });
+});
